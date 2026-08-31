061a4f6e985612440a029150c3416f5b9a7ecfc4
kierandrewett <kieran@drewett.dev>
2026-08-31 22:33:52 +0000

    test(tree-sitter): cover keyword-qualified type names

--- editors/tree-sitter-orna/src/parser.c
+++ editors/tree-sitter-orna/src/parser.c
@@ -66015,8 +66015,8 @@ TS_PUBLIC const TSLanguage *tree_sitter_orna(void) {
     .name = "orna",
     .max_reserved_word_set_size = 0,
     .metadata = {
-      .major_version = 0,
-      .minor_version = 1,
+      .major_version = 1,
+      .minor_version = 0,
       .patch_version = 0,
     },
   };
